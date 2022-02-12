08a749166948f006938a7193f175239095916c0a
Alejandro Costa <alejandroesfc@gmail.com>
2022-02-12 18:17:43 -0300

    implements get and set methods for attribute nome in classes Cliente and Banco

--- digital_bank/src/digital_bank/Banco.java
+++ digital_bank/src/digital_bank/Banco.java
@@ -9,5 +9,15 @@ public class Banco {
 	
 	/** Armazena o nome do banco.*/
 	private String nome;
+	
+	public String getNome()
+	{
+		return this.nome;
+	}
+	
+	public void setNome(String nome)
+	{
+		this.nome = nome; 
+	}
 
 }
--- digital_bank/src/digital_bank/Cliente.java
+++ digital_bank/src/digital_bank/Cliente.java
@@ -9,4 +9,15 @@ public class Cliente {
 
 	/** Armazena o nome do cliente.*/
 	private String nome;
+
+	public String getNome() 
+	{
+		return this.nome;
+	}
+
+	public void setNome(String nome) 
+	{
+		this.nome = nome;
+	}	
+	
 }
