9740648c99e37cd6e5e2b55a5e66ab8d0e30e723
Alejandro Costa <alejandroesfc@gmail.com>
2022-02-12 17:41:55 -0300

    defines attributes agencia and numeroDaConta

--- digital_bank/src/digital_bank/ContaPoupanca.java
+++ digital_bank/src/digital_bank/ContaPoupanca.java
@@ -1,5 +1,14 @@
 package digital_bank;
 
+/** 
+ * Implementa uma conta do tipo poupança.
+ * @author Alejandro Costa
+ * @version 0.1 2022-02-22
+ * */
 public class ContaPoupanca {
 
+	/** Armazena o número da agência da Conta Poupança.*/
+	private int agencia;
+	/** Armazena o número da Conta Poupança.*/
+	private int numeroDaConta;
 }
