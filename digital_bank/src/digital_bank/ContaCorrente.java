aa5d7d289b70e8d91f36f14ba59fccc51fdeb851
Alejandro Costa <alejandroesfc@gmail.com>
2022-02-12 17:43:13 -0300

    defines attributes agencia and numeroDaConta

--- digital_bank/src/digital_bank/ContaCorrente.java
+++ digital_bank/src/digital_bank/ContaCorrente.java
@@ -1,5 +1,14 @@
 package digital_bank;
 
+/** 
+ * Implementa uma conta do tipo corrente.
+ * @author Alejandro Costa
+ * @version 0.1 2022-02-22
+ * */
 public class ContaCorrente {
 
+	/** Armazena o número da agência da Conta Corrente.*/
+	private int agencia;
+	/** Armazena o número da Conta Corrente.*/
+	private int numeroDaConta;
 }
