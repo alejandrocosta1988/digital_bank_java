8b06035ee7d647d88ef5719f2b7ba18bdba8cf60
Alejandro Costa <alejandroesfc@gmail.com>
2022-02-12 19:01:47 -0300

    creates a constructor for class Conta using a sequential value for its numeroDaConta and a default number for attribute agencia

--- digital_bank/src/digital_bank/Conta.java
+++ digital_bank/src/digital_bank/Conta.java
@@ -1,5 +1,7 @@
 package digital_bank;
 
+import static digital_bank.Conta.SEQUENCIAL;
+
 /** 
  * Implementa uma conta no banco. A classe conta é abstrata porque não é de interesse que ela seja instanciada. Apenas as classes filhas ContaCorrente e ContaPoupanca deverão ser instanciadas.
  * Os atributos agencia, numeroDaConta e saldo possuem visibilidade protected. Dessa forma eles podem ser acessados a partir das classes filhas.
@@ -8,7 +10,8 @@ package digital_bank;
  */
 public abstract class Conta {
 	
-	protected static final  int AGENCIA_PADRAO = 1;
+	private static final  int AGENCIA_PADRAO = 1;
+	private static int SEQUENCIAL = 1;
 
 	/** Armazena o número da agência da Conta Corrente.*/
 	protected int agencia;
@@ -16,6 +19,15 @@ public abstract class Conta {
 	protected int numeroDaConta;
 	/** Armazena o saldo da Conta Corrente.*/
 	protected double saldo;
+	
+	
+	public Conta()
+	{
+
+		this.agencia = AGENCIA_PADRAO;
+		this.numeroDaConta = SEQUENCIAL++;
+		
+	}
 	
 	/** 
 	 * Retorna o número da agência.
--- digital_bank/src/digital_bank/ContaCorrente.java
+++ digital_bank/src/digital_bank/ContaCorrente.java
@@ -6,17 +6,5 @@ package digital_bank;
  * @version 0.1 2022-02-22
  * */
 public class ContaCorrente extends Conta {
-
-	// Precisamos de um atributo static para ir incrementando o número da conta.
-	private static int SEQUENCIAL = 1;
-	
-	public ContaCorrente()
-	{
-		// Mesma agência para todas as contas.
-		super.agencia = Conta.AGENCIA_PADRAO;
-		
-		// Sempre que uma nova conta for criada, o seu número será o número da conta criada anteriormente acrescido de 1. SEQUENCIAL, por sua vez, deverá ser incrementado de 1.
-		super.numeroDaConta = SEQUENCIAL++;
-	}
 	
 }
