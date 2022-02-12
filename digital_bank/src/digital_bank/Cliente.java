7ab88c1a3a7f40f5a919dac68a1dcc0bccaada85
Alejandro Costa <alejandroesfc@gmail.com>
2022-02-12 17:49:36 -0300

    defines attribute nome

--- digital_bank/src/digital_bank/Cliente.java
+++ digital_bank/src/digital_bank/Cliente.java
@@ -1,5 +1,12 @@
 package digital_bank;
 
+/** 
+ * Implementa um cliente.
+ * @author Alejandro Costa
+ * @version 0.1 2022-02-22
+ * */
 public class Cliente {
 
+	/** Armazena o nome do cliente.*/
+	private String nome;
 }
