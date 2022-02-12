e31677f2b025c6f9bbc39ed4f7adee24879244f9
Alejandro Costa <alejandroesfc@gmail.com>
2022-02-12 17:46:16 -0300

    creates class Banco and defines attribute nome

---
+++ digital_bank/src/digital_bank/Banco.java
@@ -0,0 +1,13 @@
+package digital_bank;
+
+/** 
+ * Implementa o Banco Digital.
+ * @author Alejandro Costa
+ * @version 0.1 2022-02-22
+ * */
+public class Banco {
+	
+	/** Armazena o nome do banco.*/
+	private String nome;
+
+}
