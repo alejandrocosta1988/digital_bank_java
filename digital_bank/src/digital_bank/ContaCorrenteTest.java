31b0959b412e90d72939010195b86f341ea98ee0
Alejandro Costa <alejandroesfc@gmail.com>
2022-02-14 06:29:46 -0300

    tests constructor with no initial deposit value

---
+++ digital_bank/src/digital_bank/ContaCorrenteTest.java
@@ -0,0 +1,23 @@
+package digital_bank;
+
+/** Define testes unitários para classe ContaCorrente
+ * 
+ */
+public class ContaCorrenteTest {
+
+	public static void main(String[] args) {
+		
+		Banco umBanco = new Banco("Banco Teste");
+		Cliente clienteUm = new Cliente();
+        clienteUm.setNome("Alejandro Costa");
+        
+        Cliente clienteDois = new Cliente();
+        clienteDois.setNome("Jana R Santana");
+        
+		System.out.println("Testando o construtor SEM valor de depósito...");
+		Conta contaUm = new ContaCorrente(clienteUm, umBanco);
+        System.out.println("OK");
+
+	}
+
+}
