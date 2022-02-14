54c253c303930276749166fde88ff6c1aa701310
Alejandro Costa <alejandroesfc@gmail.com>
2022-02-14 06:02:24 -0300

    implements tests for class Banco

---
+++ digital_bank/src/digital_bank/BancoTest.java
@@ -0,0 +1,18 @@
+package digital_bank;
+
+public class BancoTest {
+
+	/** Testes unitários para a classe Banco 
+	 * @author Alejandro Costa
+	 * @since 0.1
+	 * @version 0.1 2022-02-14
+	 */
+	public static void main(String[] args) {
+		
+        System.out.println("Testando o construtor...");
+        Banco umBanco = new Banco("Banco Teste 1");
+        System.out.println("OK");
+
+	}
+
+}
